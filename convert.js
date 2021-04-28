dcf04a6e4937317ac75582f80eeb9ca5e7226d3e
André <andreglock@gmail.com>
2021-04-28 15:59:36 +0200

    improved and added comments

--- convert.js
+++ convert.js
@@ -1,10 +1,10 @@
 let startOfCycle = 1617580800; // start of Gregorian-Bangwa cycle (Monday April 5)
-let timestamp = 1619615720; // current UNIX timestamp in seconds
-let xx = new Date();
-xx.setTime(timestamp*1000); // javascript timestamps are in milliseconds
-let UTCString = xx.toUTCString().slice(0, 16);
+let timestamp = 1624456651; // date to convert in UNIX timestamp in seconds
+// timestamp = new Date()/1000; // JavaScript function to fetch current date
 
 const convert = (currentTimestamp) => {
+    let UTCString = new Date(timestamp*1000).toUTCString().slice(0, 16); // display given time in string
+
     let dayOfCycle = ((currentTimestamp - startOfCycle) - (currentTimestamp - startOfCycle) % 86400)/ 86400;
     let dayOfWeek;
     switch (dayOfCycle % 8) {
@@ -13,21 +13,25 @@ const convert = (currentTimestamp) => {
             break;
         case 1:
             dayOfWeek = 'Anzoah';
+            break;
         case 2:
             dayOfWeek = 'Alena';
             break;
         case 3:
             dayOfWeek = 'Amina';
+            break;
         case 4:
             dayOfWeek = 'Afeah';
             break;
         case 5:
             dayOfWeek = 'Agong';
+            break;
         case 6:
             dayOfWeek = 'Aseih';
             break;
         case 7:
             dayOfWeek = 'AlungAnzoah';   
+            break;   
     }
 
     return `Today ${UTCString} is ${dayOfWeek} in the Bangwa week`;
